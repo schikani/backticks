40b87624a01935becf30b2d49616b00202fac734
Shivang Chikani <shivangchikani1@gmail.com>
2022-04-04 11:03:45 +0530

    Update lists.py

--- backticks/utils/lists.py
+++ backticks/utils/lists.py
@@ -1,8 +1,11 @@
 from backticks._tokens import *
 
 def new_list(_type, var, _list, _len=False):
+    # print(_list)
     str_to_ret = ""
     _list_len = len(_list)
+    print(f"LEN: {_list_len}")
+    # _header = f"{_type} {var}[{_list_len}];\n"
     _li = str(_list)
     _li = _li.replace(LEFTSQUARE, LEFTCURL)
     _li = _li.replace(RIGHTSQUARE, RIGHTCURL)
@@ -10,15 +13,19 @@ def new_list(_type, var, _list, _len=False):
     if _len:
         str_to_ret += f"{_type} {var}[{_len}] = {_li};\n"
 
-        return (f"{_type} {var}[{_list_len}];\n", str_to_ret)
+        # return (_header, str_to_ret)
+        return str_to_ret
+
 
     else:
         str_to_ret += f"{var}_len = {_list_len};\n"
-        str_to_ret += f"{_type} *{var} = ({_type} *)calloc({_list_len}, sizeof({_type}));\n"
+        str_to_ret += f"{var} = ({_type} *)calloc({_list_len}, sizeof({_type}));\n"
         str_to_ret += f"{var}_copy = {var};\n"
-        for i in _list_len:
+        for i in range(_list_len):
             str_to_ret += f"{var}[{i}] = {_list[i]};\n"
 
-        return (f"{_type} *{var};\n{_type} *{var}_copy;\nsize_t {var}_len;\n", str_to_ret)
+        # return (_header, str_to_ret)
+        return str_to_ret
+
 
 # def append_to_list(var, _val):
